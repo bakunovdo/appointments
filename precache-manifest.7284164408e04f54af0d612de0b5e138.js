85db93d8969bf8ea94f7b31f4fb43fb9e48fc403
bakunovdo <bakunov.do@gmail.com>
2020-10-08 16:08:05 +0300

    Updates

---
+++ precache-manifest.7284164408e04f54af0d612de0b5e138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4928cc9868e08b1c24dc51661b66aba5",
+    "url": "/appointments/index.html"
+  },
+  {
+    "revision": "d627040c76896924d189",
+    "url": "/appointments/static/css/main.c0098ee8.chunk.css"
+  },
+  {
+    "revision": "c185bc906dab1ad841ce",
+    "url": "/appointments/static/js/2.fc511ec8.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/appointments/static/js/2.fc511ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d627040c76896924d189",
+    "url": "/appointments/static/js/main.3df4e043.chunk.js"
+  },
+  {
+    "revision": "1ed2082ba097d1892d39",
+    "url": "/appointments/static/js/runtime-main.697fa79b.js"
+  }
+]);
